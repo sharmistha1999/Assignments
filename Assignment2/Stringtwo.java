ec92ea561f19bbb156f7db89fb4ba002a21373e9
sharmistha1999 <84120187+sharmistha1999@users.noreply.github.com>
2021-05-19 15:20:30 +0530

    Add files via upload

---
+++ Assignment2/Stringtwo.java
@@ -0,0 +1,26 @@
+package Assignment2;
+
+public class Stringtwo {
+	public static void removeDuplicates(String str)   
+    {      
+        String newstr = new String();  
+        int length = str.length(); 
+        for (int i = 0; i < length; i++)    
+        { 
+            char charAtPosition = str.charAt(i);
+            if (newstr.indexOf(charAtPosition) < 0)   
+            {   
+                newstr += charAtPosition;   
+            }   
+        }
+        System.out.println(newstr);  
+    }   
+    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello have a good day"; 
+        removeDuplicates(str);   
+	}
+
+}
